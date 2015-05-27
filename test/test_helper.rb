c04c731869dac9623556f08751e03111c340d1a1
Josh Cass <joshuacass@gmail.com>
2015-05-27 09:14:31 -0600

    added db cleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,21 @@ require 'minitest/autorun'
 require 'capybara'
 
 Capybara.app = TrafficSpy::Server
+
+DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
+
+class FeatureTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    TaskManager
+  end
+
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+end
