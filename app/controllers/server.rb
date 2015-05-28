3d19f62026ec53bb3936e42c80767ff2c2458a97
Josh Cass <joshuacass@gmail.com>
2015-05-28 15:32:33 -0600

    refactored and passing tests

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -10,35 +10,38 @@ module TrafficSpy class Server < Sinatra::Base
   end
 
   post '/sources/:identifier/data' do |identifier|
-    if params[:payload] == "" || params[:payload] == {}
-      status 400
-      body "Payload cannot be empty"
-    elsif TrafficSpy::Source.find_by(identifier: identifier).nil?
-      status 403
-      body "Identifier does not exist"
-    else
-      parsed_params = JSON.parse(params[:payload])
-      request = TrafficSpy::Payload.new(url: parsed_params["url"],
-                                        requested_at: parsed_params["requestedAt"],
-                                        responded_in: parsed_params["respondedIn"],
-                                        referred_by: parsed_params["referredBy"],
-                                        request_type: parsed_params["requestType"],
-                                        event_name: parsed_params["eventName"],
-                                        user_agent: parsed_params["userAgent"],
-                                        resolution_width: parsed_params["resolutionWidth"],
-                                        resolution_height: parsed_params["resolutionHeight"],
-                                        ip: parsed_params["ip"],
-                                        source_id: TrafficSpy::Source.find_by(identifier: identifier).id,
-                                        sha: Digest::SHA1.hexdigest(params[:payload]))
-
-      if request.save
-        status 200
-        body "Payload successfully received"
-      elsif request.errors.values.include?(["has already been taken"])
-        status 403
-        body "Payload already exists"
-      end
-    end
+    payload = TrafficSpy::PayloadCreator.new(params, identifier)
+    status payload.status
+    body payload.body
+    # if params[:payload] == "" || params[:payload] == {}
+    #   status 400
+    #   body "Payload cannot be empty"
+    # elsif TrafficSpy::Source.find_by(identifier: identifier).nil?
+    #   status 403
+    #   body "Identifier does not exist"
+    # else
+    #   parsed_params = JSON.parse(params[:payload])
+    #   request = TrafficSpy::Payload.new(url: parsed_params["url"],
+    #                                     requested_at: parsed_params["requestedAt"],
+    #                                     responded_in: parsed_params["respondedIn"],
+    #                                     referred_by: parsed_params["referredBy"],
+    #                                     request_type: parsed_params["requestType"],
+    #                                     event_name: parsed_params["eventName"],
+    #                                     user_agent: parsed_params["userAgent"],
+    #                                     resolution_width: parsed_params["resolutionWidth"],
+    #                                     resolution_height: parsed_params["resolutionHeight"],
+    #                                     ip: parsed_params["ip"],
+    #                                     source_id: TrafficSpy::Source.find_by(identifier: identifier).id,
+    #                                     sha: Digest::SHA1.hexdigest(params[:payload]))
+    #
+    #   if request.save
+    #     status 200
+    #     body "Payload successfully received"
+    #   elsif request.errors.values.include?(["has already been taken"])
+    #     status 403
+    #     body "Payload already exists"
+    #   end
+    # end
   end
 
 
