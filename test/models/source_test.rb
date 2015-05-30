f54791cd977f9c5044d3ca3493989abd86a5cd65
androidgrl <kawaharajamie@gmail.com>
2015-05-30 15:40:14 -0600

    Adding more source tests

--- test/models/source_test.rb
+++ test/models/source_test.rb
@@ -7,4 +7,86 @@ class SourceTest < ModelTest
 
     assert_equal [["http://jumpstartlab.com/blog", 3], ["http://jumpstartlab.com/about", 1]], source.most_visited_urls
   end
+
+  def test_average_response_times
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal [92, 35], source.average_response_times.map {|x,y| y.to_i}
+  end
+
+  def test_user_agents_when_path_is_not_entered_as_parameter
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal ["Chrome", "Chrome", "Internet Explorer", "Chrome"], source.user_agents.map {|agent| agent.browser}
+  end
+
+  def test_user_agents_when_path_is_entered_as_parameter
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal ["Chrome", "Internet Explorer", "Chrome"], source.user_agents("http://jumpstartlab.com/blog").map { |agent| agent.browser }
+  end
+
+  def test_browser_breakdown
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal "{\"Chrome\"=>3, \"Internet Explorer\"=>1}", source.browser_breakdown.to_s
+  end
+
+  def test_os_breakdown
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal "{\"OS X 10.8.2\"=>3, \"Windows 7\"=>1}", source.os_breakdown.to_s
+  end
+
+  def test_screen_res_breakdown
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal [["1920", "1280"], ["800", "600"]], source.screen_res_breakdown.map {|x,y| x }
+  end
+
+  def test_longest_response
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal 93, source.longest_response("http://jumpstartlab.com/blog")
+  end
+
+  def test_shortest_response
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal 93, source.longest_response("http://jumpstartlab.com/blog")
+  end
+
+  def test_average_response
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal 92, source.average_response("http://jumpstartlab.com/blog")
+  end
+
+  def test_http_verbs
+    source = create_source("jumpstartlab", "http://jumpstartlab.com")
+    create_payloads
+
+    assert_equal ["GET", "POST"], source.http_verbs("http://jumpstartlab.com/blog")
+  end
+
+  def test_top_referrers
+    skip
+  end
+
+  def test_top_browsers
+    skip
+  end
+
+  def test_top_os
+    skip
+  end
 end
